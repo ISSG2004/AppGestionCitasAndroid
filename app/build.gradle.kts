3f75fcbbaf75061926f7bb7e7b053350a73f019f
ISSG2004 <ismaelsangon1234@gmail.com>
2025-03-27 21:06:30 +0100

    Añadiendo dependencias y configuraciones para el google json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -36,6 +37,7 @@ android {
 }
 
 dependencies {
+    implementation (libs.play.services.auth)
     implementation (libs.firebase.auth.ktx )
     implementation (libs.firebase.database)
     implementation(libs.androidx.core.ktx)
@@ -43,6 +45,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
