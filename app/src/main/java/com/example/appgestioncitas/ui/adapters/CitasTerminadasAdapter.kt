966815971d721caaa0a603028e37fcc9299eb833
ISSG2004 <ismaelsangon1234@gmail.com>
2025-06-05 23:43:10 +0200

    Añadir hilo para cargar el método que actualice las citas pasadas

--- app/src/main/java/com/example/appgestioncitas/ui/adapters/CitasTerminadasAdapter.kt
+++ app/src/main/java/com/example/appgestioncitas/ui/adapters/CitasTerminadasAdapter.kt
@@ -1,31 +1,41 @@
 package com.example.appgestioncitas.ui.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Adapter
-import com.example.appgestioncitas.R
 import com.example.appgestioncitas.data.repository.RepositoryNegocio
 import com.example.appgestioncitas.databinding.CitaLayoutBinding
 import com.example.appgestioncitas.models.Cita
-import com.example.appgestioncitas.ui.adapters.CitasNegocioAdapter.MainHolder
 
-class CitasPendientesAdapter(
-    var listaCitas: List<Cita>,
+class CitasTerminadasAdapter(
+    private var listaCitas: List<Cita>,
     private val onItemClick: (Cita) -> Unit
-) : RecyclerView.Adapter<CitasPendientesAdapter.CitasPendientesHolder>() {
+    ): RecyclerView.Adapter<CitasTerminadasAdapter.CitasPasadasHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CitasPasadasHolder {
+        val binding = CitaLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CitasPasadasHolder(binding)
+    }
 
-    class CitasPendientesHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private var binding = CitaLayoutBinding.bind(itemView)
-        fun bind(cita: Cita,onItemClick: (Cita) -> Unit) {
-            //val repository = RepositoryNegocio()
+    override fun onBindViewHolder(holder: CitasPasadasHolder, position: Int) {
+        holder.bind(listaCitas[position], onItemClick)
+    }
 
+    override fun getItemCount(): Int = listaCitas.size
+
+    fun actualizarDatos(nuevaLista: List<Cita>) {
+        listaCitas = nuevaLista
+        notifyDataSetChanged()
+    }
+
+    class CitasPasadasHolder(private val binding: CitaLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(cita: Cita, onItemClick: (Cita) -> Unit) {
             // Procesar fecha y hora
             val fechaHora = cita.fecha_cita
             if (fechaHora != null && fechaHora.contains("T")) {
                 val (fecha, horaCompleta) = fechaHora.split("T")
-                val hora = horaCompleta.take(5) // Solo HH:mm
+                val hora = horaCompleta.take(5) // HH:mm
                 binding.tvFechaCita.text = fecha
                 binding.tvHoraFecha.text = hora
             } else {
@@ -40,27 +50,11 @@ class CitasPendientesAdapter(
                     binding.tvNegocioCita.text = negocio?.nombre ?: "Negocio no encontrado"
                 }
             }
+
             binding.btnAnular.setOnClickListener {
                 onItemClick(cita)
             }
         }
-
-
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CitasPendientesHolder {
-        val vista= LayoutInflater.from(parent.context).inflate(R.layout.cita_layout,parent,false)
-        return CitasPendientesHolder(vista)
-    }
-
-    override fun getItemCount(): Int = listaCitas.size
-
-    override fun onBindViewHolder(holder: CitasPendientesHolder, position: Int) {
-        holder.bind(listaCitas[position],onItemClick)
-    }
-
-    fun actualizarDatos(nuevaLista: List<Cita>) {
-        listaCitas = nuevaLista
-        notifyDataSetChanged()
-    }
 }
